908cfdceeb1dc075791c59426810ffeb8f2bbc53
Josue Enmanuel Poot Mateo <josue.poot@tecdesoftware.edu.mx>
2026-06-02 08:57:03 -0600

    Init: Agregar archivos del proyecto

---
+++ Models/Medico.cs
@@ -0,0 +1,11 @@
+namespace CitasApp.Models;
+
+public class Medico
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Apellido { get; set; }
+    public string Especialidad { get; set; }
+    public int NUmeroLicencia { get; set; }
+    
+}
